6463977ea7fea95d71a59428e41ca6e801c4e09c
Granjero12 <jeronimo.1228@docet.org.mx>
2024-06-15 10:46:51 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+//https://teachablemachine.withgoogle.com/models/iu-gNX9mj/
+Webcam.attach( '#camera' );
+
+camera = document.getElementById("camera");
+
+Webcam.set({
+width:350,
+heigt:300,
+image_format: 'png',
+png_quality:90
+});
+
+function take_snapshot(){
+Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+});
+}   
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/iu-gNX9mj/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
+
+function ckeck() {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+}
+
+
+function gotResult(error, results) {
+    if (error) {
+      console.error(error);      
+    } else {
+        console.log(results);
+
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
